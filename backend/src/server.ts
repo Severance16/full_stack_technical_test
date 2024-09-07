39da73d55d95fb1f9d22bfa800dbc164e1b2ee86
Sergio Ortiz <sergio.380@outlook.com>
2024-09-07 17:38:13 -0500

    feat: policy implemented - CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,16 +4,18 @@ import { connectDB } from "./config/db";
 import tasktRoutes from "./routes/taskRoutes"
 import authRoutes from "./routes/authRoutes"
 import delegateRoutes from "./routes/delegateRoutes"
+import { corsConfig } from "./config/cors";
+import cors from "cors"
 
 // Importar configuracion para uso de variables de entorno
 dotenv.config();
 
 // Llamado a la funcion para conexion a base de datos
 connectDB()
-express
 
 
 const app = express();
+app.use(cors(corsConfig))
 
 // Habilitar lectura de datos en formato JSON
 app.use(express.json())
